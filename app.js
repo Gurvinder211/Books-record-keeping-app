a97cf9e711df88132180204beea804fa8c891536
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2025-03-02 16:01:42 +0530

    added routes folder

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express')
+const express = require('express');
 const app = express()
-const port = 3000
+const port = 3000;
+const login = require('./routes/login');
+
+app.use('/login', login);
 
 app.get('/', (req, res) => {
-  res.send('Book record keeping web app')
+  res.send('Welcome to the book record keeping web app!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
