d4773814742f416f582b342ac561651fabdaf4ac
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2025-03-18 20:48:27 +0530

    created the front page, linked css file

--- app.js
+++ app.js
@@ -11,15 +11,16 @@ connectDB();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+ 
 
 
 
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the book record keeping web app!')
+app.get('/', (req, res) => { 
+  res.render('home.ejs')
 })
 
-
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
